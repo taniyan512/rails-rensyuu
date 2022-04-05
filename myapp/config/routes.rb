adbb880dfbdb1c21a5c34ddf2f37efb0da171cf4
EC2 Default User <ec2-user@ip-172-31-28-163.ec2.internal>
2022-04-05 22:25:50 +0000

    ポテパンカリキュラム練習

---
+++ myapp/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'lessons/hello', to: 'lessons#hello'
+  get 'lessons/call', to: 'lessons#call'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+end
